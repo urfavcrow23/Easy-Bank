f1839714b3fbf32c7863b23b4e29df056019c310
Karim El Refai <kariemasaad992@gmail.com>
2024-09-28 21:43:44 +0300

    #

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,11 @@
 import PropTypes from "prop-types";
 
-function Button({ label, backgroundColor = "red", size = "md", handleClick }) {
+function Button({ label, backgroundColor = "", size = "md", handleClick }) {
   let scale = 1;
   if (size === "sm") scale = 0.75;
   if (size === "lg") scale = 1.5;
   const style = {
+    // text-white rounded-full  p-2 w-40
     backgroundColor,
     padding: `${scale * 0.5}rem ${scale * 1}rem`,
     border: "none",
